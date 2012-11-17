88de231cbfc1c56074fad82d4794d082e4bcd361
Razican <admin@razican.com>
2012-11-17 19:52:12 +0100

    Se borra el código antiguo.

--- styles/views/adm/SQLerrorMessagesBody.php
+++ styles/views/adm/SQLerrorMessagesBody.php
@@ -1,18 +1,3 @@
-<body>
-<table width="70%">
-	<tr>
-		<td class="c" colspan="5">{er_sql_error_list} [<a href="?page=sql&deleteall=yes">{er_dlte_all}</a>]</td>
-	</tr>
-	<tr>
-		<td class="c" width="25">{input_id}</td>
-		<td class="c" width="70">{er_user}</td>
-		<td class="c" width="100">{er_type}</td>
-		<td class="c" width="230">{er_date}</td>
-		<td class="c" width="95">{button_delete}</td>
-	</tr>
-	{errors_list}
-</table>
-</body>
 <section class="page">
 	<section class="content-table">
 		<h3>{er_sql_error_list} [<a href="admin.php?page=errors&amp;errors=sql&amp;deleteall=yes">{er_dlte_all}</a>]</h3>
